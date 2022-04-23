5d4d477fbb9522b093b829744e7f567b9203811b
ehsan telgerdy <telgerdyehsan@gmail.com>
2022-04-23 18:18:25 +0000

    sentiment creator modified

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -22,6 +22,17 @@ function getNLUInstance() {
     /*Type the code to create the NLU instance and return it.
     You can refer to the image in the instructions document
     to do the same.*/
+     const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2021-08-01',
+        authenticator: new IamAuthenticator ({
+            apikey: api_key
+        }),
+        serviceUrl: api_url
+    });
+    return naturalLanguageUnderstanding;
 }
 
 
@@ -125,12 +136,11 @@ app.get("/text/sentiment", (req,res) => {
     }
 
     const naturalLanguageUnderstanding = getNLUInstance();
-    return res.send("naturalLanguageUnderstanding",null,2);
     naturalLanguageUnderstanding.analyze(analyzeParams)
     .then(analysisResults => {
         //Retrieve the sentiment and return it as a formatted string
 
-        return res.send(analysisResults.result.keywords[0].sentiment,null,2);
+        return res.send(analysisResults.result.keywords,null,2);
     })
     .catch(err => {
         return res.send("Could not do desired operation "+err);
